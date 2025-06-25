5fecacc9e10bc20dc92fdff9883033e795875735
VishnuKaku <151540923+VishnuKaku@users.noreply.github.com>
2025-06-25 13:42:21 +0530

    Update Problem-4.js

--- Problem-4.js
+++ Problem-4.js
@@ -1,1 +1,10 @@
-// 4th code
+function countMultiples(numbers) {
+    const result = {};
+    for (let i = 1; i <= 9; i++) {
+        result[i] = numbers.filter(num => num % i === 0).length;
+    }
+    return result;
+}
+
+const numbers = [1, 2, 8, 9, 12, 46, 76, 82, 15, 20, 30];
+console.log(countMultiples(numbers));
