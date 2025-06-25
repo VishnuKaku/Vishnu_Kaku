bdf32aeae6727addba534ab1fb9cafd57e0c7c68
VishnuKaku <151540923+VishnuKaku@users.noreply.github.com>
2025-06-25 13:40:09 +0530

    Update Problem-3.js

--- Problem-3.js
+++ Problem-3.js
@@ -1,1 +1,15 @@
-//3rd code
+function generateSeries(a) {
+    const n = a % 2 === 1 ? a : a - 1;
+    const result = [];
+    for (let i = 0; i < n; i++) {
+        result.push(2 * i + 1);
+    }
+    return result;
+}
+
+console.log(generateSeries(1));
+console.log(generateSeries(2));
+console.log(generateSeries(3));
+console.log(generateSeries(4));
+console.log(generateSeries(5));
+console.log(generateSeries(6));
