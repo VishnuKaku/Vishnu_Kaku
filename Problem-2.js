224ca9d81344e3a761825787d2e108972b619a1e
VishnuKaku <151540923+VishnuKaku@users.noreply.github.com>
2025-06-25 13:36:53 +0530

    Update Problem-2.js

--- Problem-2.js
+++ Problem-2.js
@@ -1,1 +1,11 @@
-//2nd code
+function generateOddSeries(a) {
+    const result = [];
+    for (let i = 0; i < a; i++) {
+        result.push(2 * i + 1);
+    }
+    return result;
+}
+
+console.log(generateOddSeries(1));
+console.log(generateOddSeries(3));
+console.log(generateOddSeries(4));
